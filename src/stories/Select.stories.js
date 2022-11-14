0a6282fc2285c0109920f3b88a4901455ed7e3d2
CharlesVeneau <veneauc@hotmail.fr>
2022-11-14 21:39:00 +0100

    fix style issues on select

--- src/stories/Select.stories.js
+++ src/stories/Select.stories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { storiesOf } from "@storybook/react";
 
 import { Select } from '../components/Select'
@@ -8,7 +8,25 @@ import DATA from '../DATA.json'
 const stories = storiesOf('App test', module)
 
 stories.add("App", () => {
+      const userMockup = {
+    id: 1,
+    firstName: null,
+    lastName: null,
+    dateofBirth: null,
+    street: null,
+    city: null,
+    stateAbbrev: null,
+    zipCode: null,
+    startDate: null,
+    department: null,
+  };
+  const [userInfo, setUserInfo] = useState(userMockup);
+
+    function handleChange(event, name) {
+    setUserInfo({ ...userInfo, [name]: event });
+  }
+
     return (
-        <Select data={DATA} name="States"/>
+        <Select data={DATA} name="States" handleChange={handleChange}/>
     )
 })
